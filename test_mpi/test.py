7fffb067034b04e5f7a985b852c6e72dc6d0b5c3
Stefano Tagliaventi <s.tagliaventi@cineca.it>
2018-12-07 18:26:30 +0100

    added test

---
+++ test_mpi/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from mpi4py import MPI
+
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+name = MPI.Get_processor_name()
+
+print("host -- > ",name)
+
+
+
+
+
+
--- train_mnist.py
+++ train_mnist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3 
 
 from mpi4py import MPI
 import argparse
@@ -6,7 +6,10 @@ import sys
 import os
 
 
-import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+import tensorflow.app 
+import tensorflow.train 
 
 FLAGS = None
 
@@ -14,11 +17,17 @@ def main(_):
   ps_hosts = FLAGS.ps_hosts.split(",")
   worker_hosts = FLAGS.worker_hosts.split(",")
   # Create a cluster from the parameter server and worker hosts.
-  cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
+  cluster = tensorflow.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
   # Create and start a server for the local task.
-  server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
+  server = tensorflow.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
 
-  print "I'm the",FLAGS
+  print ("I'm the",FLAGS)
+  print ("ps_hosts",ps_hosts)
+  print ("worker_hosts",worker_hosts)
+
+  # Import data
+  mnist = input_data.read_data_sets("train")
+
 
   if FLAGS.job_name == "ps":
     server.join()
@@ -29,25 +38,25 @@ def main(_):
     with tf.device(tf.train.replica_device_setter(worker_device=worker_device, cluster=cluster)):
       # Build model...
       y_  = tf.Variable(tf.zeros([1]))
-      print "---------------->", y_
+      print ("---------------->", y_)
 #      y   = tf.placeholder(tf.float32,[None,10])
 #      loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
       loss = tf.reduce_mean(y_)
-      global_step = tf.contrib.framework.get_or_create_global_step()
+      global_step = tf.train.get_or_create_global_step()
       train_op = tf.train.AdagradOptimizer(0.01).minimize(loss, global_step=global_step)
     # The StopAtStepHook handles stopping after running given steps.
     hooks=[tf.train.StopAtStepHook(last_step=2)]
     # The MonitoredTrainingSession takes care of session initialization,
     # restoring from a checkpoint, saving to a checkpoint, and closing when done
     # or an error occurs.
-    with tf.train.MonitoredTrainingSession(master=server.target, is_chief=(FLAGS.task_index == 0), checkpoint_dir="/tmp/train_logs", hooks=hooks) as mon_sess:
+    with tf.train.MonitoredTrainingSession(master=server.target, is_chief=(FLAGS.task_index == 0), checkpoint_dir="/galileo/home/userinternal/stagliav/tf_distributed/train_logs", hooks=hooks) as mon_sess:
       while not mon_sess.should_stop():
         # Run a training step asynchronously.
         # See `tf.train.SyncReplicasOptimizer` for additional details on how to
         # perform *synchronous* training.
         # mon_sess.run handles AbortedError in case of preempted PS.
         mon_sess.run(train_op)
-    print y_
+  print ("-<-<-<->",y_[0])
 
 
 
@@ -58,6 +67,10 @@ if __name__ == "__main__":
   name = MPI.Get_processor_name()
 
   dt = {'host':name,'rank':rank,'local_rank':0,'type':'ps','task':0}
+
+
+  print("hostname:",name," number of processes:",size)
+
  
   # build local rank
   nodes = []
@@ -91,8 +104,8 @@ if __name__ == "__main__":
   worker_list = []
   for (ind,vv) in enumerate(dt):
     port=4444
-    if dt[ind]['type']=='worker':
-      worker_list.append(vv.get("host")+':'+str(port))
+    #if dt[ind]['type']=='worker':
+    worker_list.append(vv.get("host")+':'+str(port))
   worker_list=str(worker_list).strip("[]").replace('\'','') 
 
   parser = argparse.ArgumentParser()
@@ -106,12 +119,11 @@ if __name__ == "__main__":
   FLAGS.ps_hosts     = ps_list
   FLAGS.worker_hosts = worker_list
 
-  print "ps:",FLAGS.ps_hosts
-  print "worker:",FLAGS.worker_hosts
+  print ("ps:",FLAGS.ps_hosts)
+  print ("worker:",FLAGS.worker_hosts)
 
   FLAGS.job_name   = dt[rank]['type']
   FLAGS.task_index = dt[rank]['task'] 
 
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
-
+  tensorflow.app.run(main=main, argv=[sys.argv[0]] + unparsed)
 
